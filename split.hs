7b2748dcb4227c3029e8c142b29100e2f186e36b
NandorKovacs <nandor1kovacs@gmail.com>
2023-04-04 20:42:11 +0200

    better split with slight error

--- split.hs
+++ split.hs
@@ -1,6 +1,7 @@
-idx c s = [i | i <- [0..(length s - 1)], s !! i == c]
+split :: Char -> String -> [String]
+split c [] = []
+split c s = (next c s) : split c (drop ((length . next c) s + 1) s) 
 
-getrange l a b = take (b-a) (drop a l)
-
-split c s = if null (idx c s) then [s] else ([getrange s 0 (head (idx c s))] ++ [getrange s ((idx c s) !! (i - 1) + 1) ((idx c s) !! i) | i <- [1..(length (idx c s) - 1)]] ++ [getrange s (last (idx c s) + 1) (length s)])
-
+next :: Char -> String -> String
+next c [] = []
+next c (s:sx) = if s == c then [] else s : (next c sx)
