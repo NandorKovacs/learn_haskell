0f31be9671061c65d19cedfa67c8b22b72b17973
NandorKovacs <nandor1kovacs@gmail.com>
2023-04-06 10:53:00 +0200

    day5 compiles

--- aoc/day5.hs
+++ aoc/day5.hs
@@ -1,17 +1,17 @@
 main = do
   contents <- getContents
-  let l = lines getContents
+  let l = lines contents
   let crates = getCrates l
-  let actions = getActions
+  let actions = getActions l
   print $ topCrates crates actions
 
 getCrates :: [String] -> [[Char]]
-getCrates ((' ':'1':x):_) = take (length x + 3) repeat []
+getCrates ((' ':'1':x):_) = take (length x + 3) (repeat [])
 getCrates (x:xs) = lineToCrate x `stack` getCrates xs
 
 lineToCrate :: String -> [Char]
 lineToCrate [] = []
-lineToCrate ('[':x:']') = x : lineToCrate $ drop 4 x
+lineToCrate ('[':x:']':xs) = x : (lineToCrate $ drop 4 xs)
 lineToCrate x = lineToCrate $ drop 4 x 
 
 stack :: [Char] -> [[Char]] -> [[Char]]
@@ -22,7 +22,7 @@ getActions :: [String] -> [Action]
 getActions [] = []
 getActions (x@('m':'o':'r':'e':_):xs) = do
   let w = words x
-  (read head (drop 1 x), read head (drop 3 x), read head (drop 5 x)) : getActions 
+  Action {amount = read (head (drop 1 w)), from = read (head (drop 3 w)), to = read (head (drop 5 w))} : getActions xs
 getActions (x:xs) = getActions xs
 
 topCrates :: [[Char]] -> [Action] -> [Char]
@@ -34,5 +34,5 @@ performActions :: [[Char]] -> [Action] -> [[Char]]
 performActions a [] = a 
 performActions a (b:bs) = do
   let l = length a
-  let func i = if i == from b then take (l - amount) (a !! i) else if i == to b then a + take (l - amount) (a !! from b) else (a !! i) 
-  topCrates [func |i <- [0..l]]
+  let func i = if i == from b then take (l - amount b) (a !! i) else if i == to b then (a !! i) ++ take (l - amount b) (a !! from b) else (a !! i) 
+  performActions [func i |i <- [0..l]] bs
